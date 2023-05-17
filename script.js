1b6fee1350afe8ec05d09ac3ca168ca6ef2f255c
WebDevEren <erenefekara2006@gmail.com>
2023-05-17 10:46:04 +0300

    active class added to hamburger and listgroup

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 let listGroup = document.querySelector("#navbar-list-group");
 let hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  listGroup.classList.toggle("active");
+});
